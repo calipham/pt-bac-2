255d86747af040162d07248c52534dca921670a9
calipham <nguyenpham871@gmail.com>
2017-04-26 21:26:09 +0800

    Add files via upload

---
+++ Exer1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Exer1
+{
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner( System.in);
+        double a, b, c, x, x1, x2, formula, delta;
+        System.out.print(" Enter a= ");
+        a = sc.nextDouble();
+        System.out.print("Enter b= ");
+        b = sc.nextDouble();
+        System.out.print(" Enter c= ");
+        c = sc.nextDouble();
+        System.out.println("(a * x*x) + (b * x) + c) = 0 , a !=0");
+        delta = b*b - 4*a*c;
+        if ( delta < 0) {
+            System.out.println( " Phuong trinh vo nghiem " );
+        }
+            else if ( delta == 0 ){
+                System.out.println( " Phuong trinh co nghiem kep " );
+                x1 = x2 = -b / (2*a);
+                System.out.println( " x1 = x2 = -b / (2 *a) = " + x1 );
+            }
+            else if ( delta > 0 ){
+                System.out.println( " Phuong trinh co 2 nghiem phan biet ");
+                x1 = (-b + Math.sqrt(delta))/a;
+                System.out.println(" x1 = " + x1 );
+                x2 = (-b + Math.sqrt(delta))/a;
+                System.out.println(" x2 = " + x2);
+            }
+        }
+    }
+                
+                
+        
+        
